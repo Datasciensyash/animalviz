44c37d6156f5acb803ec24a5e36e2e797ab1bc83
Datasciensyash <gribulae@gmail.com>
2021-11-27 22:01:57 +0300

    Add interface for ranking task and visualization

---
+++ animalviz/clip_interface.py
@@ -0,0 +1,74 @@
+import dataclasses
+import typing as tp
+from pathlib import Path
+
+import clip
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+
+
+@dataclasses.dataclass
+class ClipOutput:
+    text_embedding: np.ndarray
+    image_embedding: np.ndarray
+    probability: float
+
+
+class ClipInterface:
+    def __init__(
+            self,
+            model_name: str = "RN50",
+            device: str = 'cpu'
+    ):
+        self.device = device
+        self.model, self.transform = self._load_model(model_name)
+        self.model.to(self.device)
+
+    def prepare_image(self, image_path: Path) -> torch.Tensor:
+        image = Image.open(str(image_path)).convert("RGB")
+        image = self.transform(image)
+        return image.unsqueeze(0).to(self.device)
+
+    def get_embedding(self, image_tensor: torch.Tensor):
+        if image_tensor.shape[0] != 1:
+            raise ValueError(
+                f'This function can only calculate embedding for batch_size 1, '
+                f'got {image_tensor.shape[0]}'
+            )
+        with torch.no_grad():
+            embedding = self.model(image_tensor).flatten().cpu().numpy()
+        return embedding
+
+    def get_visual_embedding(self, image_path: Path) -> np.ndarray:
+        with torch.no_grad():
+            image_tensor = self.prepare_image(image_path)
+            image_tensor = self.model.encode_image(image_tensor).float()
+            image_tensor /= image_tensor.norm(dim=-1, keepdim=True)
+            return image_tensor.detach().cpu().numpy()
+
+    def get_textual_embedding(self, text: str) -> np.ndarray:
+        with torch.no_grad():
+            tokenized = clip.tokenize([text]).to(self.device)
+            text_tensor = self.model.encode_text(tokenized).float()
+            text_tensor /= text_tensor.norm(dim=-1, keepdim=True)
+            return text_tensor.detach().cpu().numpy()
+
+    def classify_image(self, image_path: Path, text: str) -> ClipOutput:
+        image_embedding = self.get_visual_embedding(image_path)
+        text_embedding = self.get_textual_embedding(text)
+        probability = text_embedding @ image_embedding.T
+        return ClipOutput(
+            image_embedding=image_embedding,
+            text_embedding=text_embedding,
+            probability=probability
+        )
+
+    @staticmethod
+    def _load_model(
+            model_name: str
+    ) -> tp.Tuple[torch.nn.Module, torchvision.transforms.Compose]:
+        model, preprocess = clip.load(model_name)
+        model.eval()
+        return model, preprocess
--- visualize.py
+++ visualize.py
@@ -4,41 +4,74 @@ import streamlit as st
 import yaml
 from tqdm import tqdm
 
+from deep_translator import GoogleTranslator
+
+from animalviz.clip_interface import ClipInterface
 from animalviz.eval_interface import ClassificationOutput
 from animalviz.model_loading import load_from_directory
 
-path_to_model = st.sidebar.text_input('Path to model', value="./models/")
-path_to_model = Path(path_to_model)
-
-device = st.sidebar.text_input('Device', value='cpu')
+translator = GoogleTranslator(source='auto', target='en')
 
 path_to_directory = st.sidebar.text_input('Path to directory')
 files_to_predict = list(Path(path_to_directory).rglob('*.jpg'))
+device = st.sidebar.text_input('Device', value='cpu')
 
-eval_interface = load_from_directory(path_to_model, device)
+task = st.sidebar.selectbox("Choose task", ["Classification", "Ranking"])
 
-predictions = []
+if task == "Classification":
+    path_to_model = st.sidebar.text_input('Path to model', value="./models/")
+    path_to_model = Path(path_to_model)
 
-progress_bar = st.sidebar.progress(0)
-for i, file in tqdm(enumerate(files_to_predict)):
-    progress_bar.progress(i / len(files_to_predict))
+    eval_interface = load_from_directory(path_to_model, device)
 
-    meta_fname = file.with_suffix('.yml')
-    if not meta_fname.exists():
-        cls_out = eval_interface.classify_image(file)
-        with meta_fname.open('w') as file:
-            yaml.dump({
-                'label': cls_out.label,
-                'label_name': cls_out.label_name,
-                'probability': cls_out.probability
-            }, file)
-    else:
-        with meta_fname.open('r') as file:
-            meta = yaml.load(file, Loader=yaml.SafeLoader)
-            cls_out = ClassificationOutput(
-                **meta
-            )
-    predictions.append(cls_out)
-progress_bar.progress(1.)
+    predictions = []
+    progress_bar = st.sidebar.progress(0)
+    for i, filename in tqdm(enumerate(files_to_predict)):
+        progress_bar.progress(i / len(files_to_predict))
 
-st.write(predictions)
+        meta_fname = filename.with_suffix('.yml')
+        if not meta_fname.exists():
+            cls_out = eval_interface.classify_image(file)
+            with meta_fname.open('w') as file:
+                yaml.dump({
+                    'label': cls_out.label,
+                    'label_name': cls_out.label_name,
+                    'probability': cls_out.probability
+                }, file)
+        else:
+            with meta_fname.open('r') as file:
+                meta = yaml.load(file, Loader=yaml.SafeLoader)
+                cls_out = ClassificationOutput(
+                    **meta
+                )
+        predictions.append((filename, cls_out))
+    progress_bar.progress(1.)
+
+    classes = set([p[1].label_name for p in predictions])
+    cls_to_show = st.sidebar.selectbox("Select class to show", list(classes))
+    num_to_show = st.sidebar.number_input("Number of images to show", min_value=1, max_value=10, value=5)
+    predictions = [p for p in predictions if p[1].label_name == cls_to_show]
+    predictions = sorted(predictions, key=lambda x: x[1].probability, reverse=True)
+    for file, prediction in predictions:
+        st.write(f'Predicted class {cls_to_show} with probability {prediction.probability}')
+        st.image(str(file))
+
+elif task == "Ranking":
+    clip_interface = ClipInterface(device=device)
+    top_n = st.sidebar.number_input("Number of images to show", min_value=1, max_value=10, value=5)
+
+    russian_input = st.sidebar.text_input("What to find on images?", value="Лиса")
+    english_input = translator.translate(russian_input)
+
+    predictions = []
+    progress_bar = st.sidebar.progress(0)
+    for i, file in tqdm(enumerate(files_to_predict)):
+        progress_bar.progress(i / len(files_to_predict))
+        cls_out = clip_interface.classify_image(file, russian_input)
+        predictions.append((file, cls_out))
+    progress_bar.progress(1.)
+
+    predictions = sorted(predictions, key=lambda x: x[1].probability, reverse=True)
+    for i in range(min(top_n, len(predictions))):
+        st.write(f'Probability of {russian_input}: {predictions[i][1].probability}')
+        st.image(str(predictions[i][0]))
